1e04671401ed4f7428fce7d3106c66a2081c104c
lc3267 <melvin.strobl@kit.edu>
2023-08-07 16:30:55 +0200

    created standard pipelines
    Signed-off-by: lc3267 <melvin.strobl@kit.edu>

---
+++ src/quantum_siren/pipelines/postprocessing/nodes.py
@@ -0,0 +1,4 @@
+"""
+This is a boilerplate pipeline 'postprocessing'
+generated using Kedro 0.18.12
+"""
---
+++ src/quantum_siren/pipelines/preprocessing/pipeline.py
@@ -0,0 +1,10 @@
+"""
+This is a boilerplate pipeline 'preprocessing'
+generated using Kedro 0.18.12
+"""
+
+from kedro.pipeline import Pipeline, pipeline
+
+
+def create_pipeline(**kwargs) -> Pipeline:
+    return pipeline([])
