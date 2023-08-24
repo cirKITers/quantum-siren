bc956152ef7843f001023868d293f66f31511793
lc3267 <melvin.strobl@kit.edu>
2023-08-24 16:59:50 +0200

    model tracking using mlflow
    Signed-off-by: lc3267 <melvin.strobl@kit.edu>

--- src/quantum_siren/pipelines/training/nodes.py
+++ src/quantum_siren/pipelines/training/nodes.py
@@ -18,37 +18,47 @@ from .ansaetze import ansaetze
 
 import mlflow
 
-class Instructor():
-    def __init__(self, n_layers, n_qubits, vqc_ansatz, iec_ansatz, data_reupload, learning_rate, shots) -> None:
-        shots = None if shots=="None" else shots
-        dev = qml.device("default.qubit", wires=n_qubits, shots=shots)
+
+class Model(mlflow.pyfunc.PythonModel):
+    def __init__(self, n_qubits, shots, vqc_ansatz, iec_ansatz, n_layers, data_reupload) -> None:
+        self.shots = None if shots=="None" else shots
         self.n_qubits = n_qubits
+        self.n_layers = n_layers
 
         self.iec = getattr(ansaetze, iec_ansatz, ansaetze.nothing)
         self.vqc = getattr(ansaetze, vqc_ansatz, ansaetze.nothing)
-        self.qnode = qml.QNode(self.circuit, dev, interface="torch")
-
-        self.params = self.initialize_params(n_qubits=n_qubits, n_layers=n_layers, n_gates_per_layer=self.vqc(None))
-        self.optim = torch.optim.Adam(lr=learning_rate, params=[self.params])
-
-        self.steps_till_summary = 10
-
+        
         self.data_reupload = data_reupload
+
+        dev = qml.device("default.qubit", wires=self.n_qubits, shots=self.shots)
+        self.predict = qml.QNode(self.circuit, dev, interface="torch")
+
+        self.params = self.initialize_params(n_qubits=self.n_qubits, n_layers=self.n_layers, n_gates_per_layer=self.vqc(None))
 
     def initialize_params(self, n_qubits, n_layers, n_gates_per_layer):
         return torch.rand(size=(n_layers,n_qubits,n_gates_per_layer), requires_grad=True)
 
-
-    def circuit(self, params, coord):
-        for l, l_params in enumerate(params):
+    def circuit(self, model_input):
+        for l, l_params in enumerate(self.params):
             if l == 0 or (l > 0 and self.data_reupload):
-                self.iec(torch.stack([coord]*(self.n_qubits//2)), limit=self.n_qubits-(l//2)) # half because the coordinates already have 2 dims
+                self.iec(torch.stack([model_input]*(self.n_qubits//2)), limit=self.n_qubits-(l//2)) # half because the coordinates already have 2 dims
 
             self.vqc(l_params)
 
         return [qml.expval(qml.PauliZ(i)) for i in range(self.n_qubits)]
 
-    def cost(self, model_input, params):
+
+class Instructor():
+    def __init__(self, n_layers, n_qubits, vqc_ansatz, iec_ansatz, data_reupload, learning_rate, shots) -> None:
+
+
+        self.steps_till_summary = 10
+        
+        self.model = Model(n_qubits, shots, vqc_ansatz, iec_ansatz, n_layers, data_reupload)
+        self.optim = torch.optim.Adam(lr=learning_rate, params=[self.model.params])
+    
+
+    def cost(self, model_input):
         out = torch.zeros(size=[model_input.shape[0],])
 
         # with Pool(processes=4) as pool:
@@ -56,7 +66,7 @@ class Instructor():
         
         for i, coord in enumerate(model_input):
             # out[i] = torch.mean(torch.stack(circuit(params, coord)), axis=0)
-            out[i] = self.qnode(params, coord)[-1]
+            out[i] = self.model.predict(coord)[-1]
 
         return out
 
@@ -78,7 +88,7 @@ class Instructor():
 
         for step in range(steps):
 
-            model_output = self.cost(model_input[0], self.params)
+            model_output = self.cost(model_input[0])
             model_output = model_output.reshape((1, model_output.shape[0], 1))
 
             loss_val = self.mse(model_output, ground_truth)
@@ -114,6 +124,8 @@ class Instructor():
             loss_val.backward()
             self.optim.step()
 
+        return self.model
+
 
 def generate_instructor(n_layers, n_qubits, vqc_ansatz, iec_ansatz, data_reupload, learning_rate, shots):
     instructor = Instructor(n_layers, n_qubits, vqc_ansatz, iec_ansatz, data_reupload, learning_rate, shots)
@@ -141,8 +153,10 @@ def plot_ground_truth(ground_truth):
     }
 
 def training(instructor, model_input, ground_truth, steps, report_figure_every_n_steps):
-    result = instructor.train(model_input, ground_truth, steps, report_figure_every_n_steps)
+    model = instructor.train(model_input, ground_truth, steps, report_figure_every_n_steps)
+
+    mlflow.pyfunc.log_model(python_model=model, artifact_path="qameraman", input_example=model_input.numpy()[0][0])
 
     return {
-        "result": "result"
+        "result": model
     }
