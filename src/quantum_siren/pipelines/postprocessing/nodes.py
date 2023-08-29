c2ac2e765e09407d0dc6f21773adeb3b8e004816
lc3267 <melvin.strobl@kit.edu>
2023-08-29 16:44:01 +0200

    fixed upscaling node
    Signed-off-by: lc3267 <melvin.strobl@kit.edu>

--- src/quantum_siren/pipelines/postprocessing/nodes.py
+++ src/quantum_siren/pipelines/postprocessing/nodes.py
@@ -11,13 +11,20 @@ import math
 
 
 def upscaling(model, coordinates, factor):
-    upscaled_coordinates = torch.tensor(rescale(coordinates.numpy(), [factor**2, 1]))
-    sidelength = int(math.sqrt(upscaled_coordinates.shape[0]))
+    sidelength = math.sqrt(coordinates.shape[0])
+    upscaled_sidelength = int(sidelength*factor)
+
+    upscaled_coordinates = torch.zeros(size=(upscaled_sidelength**2, 2))
+    it = 0
+    for x in torch.linspace(0, coordinates.max(), upscaled_sidelength):
+        for y in torch.linspace(0, coordinates.max(), upscaled_sidelength):
+            upscaled_coordinates[it] = torch.tensor([x, y])
+            it += 1
 
     model_output = model(upscaled_coordinates)
 
     fig = go.Figure(data =
-                    go.Heatmap(z = model_output.cpu().view(sidelength, sidelength).detach().numpy())
+                    go.Heatmap(z = model_output.cpu().view(upscaled_sidelength, upscaled_sidelength).detach().numpy())
                 )
     fig.update_layout(
         yaxis=dict(
@@ -30,6 +37,6 @@ def upscaling(model, coordinates, factor):
     mlflow.log_figure(fig, f"{factor}x_upscaled_prediction.html")
 
     return {
-        "upscaled_image":model_output.numpy(),
+        "upscaled_image":model_output.detach().numpy(),
         "upscaled_coordinates":upscaled_coordinates
     }
