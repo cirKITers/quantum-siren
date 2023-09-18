e75780bdcadfc5eb68295fc853d7ca0cfbb7a9e5
lc3267 <melvin.strobl@kit.edu>
2023-09-18 11:20:40 +0200

    implemented gradients plotting
    Signed-off-by: lc3267 <melvin.strobl@kit.edu>

--- src/quantum_siren/pipelines/postprocessing/nodes.py
+++ src/quantum_siren/pipelines/postprocessing/nodes.py
@@ -8,7 +8,7 @@ import torch
 import mlflow
 import plotly.graph_objects as go 
 import math
-
+import scipy
 
 def upscaling(model, coordinates, factor):
     sidelength = math.sqrt(coordinates.shape[0])
@@ -71,28 +71,46 @@ def plot_gradients(model, coordinates, ground_truth):
     coordinates.requires_grad = True
     model_output = model(coordinates)
 
-    model_output_grad = torch.autograd.grad(outputs=model_output.sum(), inputs=coordinates, grad_outputs=None) #same shape as coordinates
+    model_output_grad = torch.autograd.grad(outputs=model_output.sum(), inputs=coordinates, grad_outputs=None)[0] #same shape as coordinates
+
+    pred_normed_grad = model_output_grad.norm(dim=-1)
+
+    fig = go.Figure(data =
+                    go.Heatmap(z = pred_normed_grad.cpu().view(sidelength, sidelength).detach().numpy())
+                )
+
+    fig.update_layout(
+        yaxis=dict(
+            scaleanchor='x',
+            autorange='reversed'
+        ),
+        plot_bgcolor='rgba(0,0,0,0)'
+    )
+
+    mlflow.log_figure(fig, f"prediction_gradients.html")
 
 
-    # for pixel in model_output:
-    #     pixel.backward(retain_graph=True)
 
-    
-    # difference = model_output - ground_truth.view(sidelength**2)
+    grads_x = scipy.ndimage.sobel(ground_truth.numpy(), axis=1).squeeze(0)[..., None]
+    grads_y = scipy.ndimage.sobel(ground_truth.numpy(), axis=2).squeeze(0)[..., None]
+    grads_x, grads_y = torch.from_numpy(grads_x), torch.from_numpy(grads_y)
+            
+    grads = torch.stack((grads_x, grads_y), dim=-1).view(-1, 2)
+    normed_grad = grads.norm(dim=-1)
 
-    # fig = go.Figure(data =
-    #                 go.Heatmap(z = difference.cpu().view(sidelength, sidelength).detach().numpy())
-    #             )
+    fig = go.Figure(data =
+                    go.Heatmap(z = normed_grad.cpu().view(sidelength, sidelength).detach().numpy())
+                )
 
-    # fig.update_layout(
-    #     yaxis=dict(
-    #         scaleanchor='x',
-    #         autorange='reversed'
-    #     ),
-    #     plot_bgcolor='rgba(0,0,0,0)'
-    # )
+    fig.update_layout(
+        yaxis=dict(
+            scaleanchor='x',
+            autorange='reversed'
+        ),
+        plot_bgcolor='rgba(0,0,0,0)'
+    )
 
-    # mlflow.log_figure(fig, f"pixelwise_differences.html")
+    mlflow.log_figure(fig, f"gradients.html")
 
     return {
     }
