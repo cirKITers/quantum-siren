b86a2a2ba5d20670198b57a9972f68cbc92c7968
lc3267 <melvin.strobl@kit.edu>
2023-09-20 09:35:37 +0200

    switched to xy dependent visualization
    Signed-off-by: lc3267 <melvin.strobl@kit.edu>

--- src/quantum_siren/pipelines/postprocessing/nodes.py
+++ src/quantum_siren/pipelines/postprocessing/nodes.py
@@ -9,6 +9,9 @@ import mlflow
 import plotly.graph_objects as go 
 import math
 import scipy
+import numpy as np
+import matplotlib.colors as colors
+import plotly.express as px
 
 def upscaling(model, coordinates, factor):
     sidelength = math.sqrt(coordinates.shape[0])
@@ -74,30 +77,34 @@ def plot_gradients(model, coordinates, ground_truth):
     #----------------------------------------------------------------    
     # Gradient Prediction
 
-    model_output_dc = torch.autograd.grad(outputs=model_output.sum(), inputs=coordinates, grad_outputs=None, create_graph=True)[0] #same shape as coordinates
+    pred_dc = torch.autograd.grad(outputs=model_output.sum(), inputs=coordinates, grad_outputs=None, create_graph=True)[0] #same shape as coordinates
 
-    pred_normed_dc = model_output_dc.norm(dim=-1)
+    pred_dc_img = grads2img(pred_dc[..., 0], pred_dc[..., 1], sidelength)
 
-    fig = go.Figure(data =
-                    go.Heatmap(z = pred_normed_dc.cpu().view(sidelength, sidelength).detach().numpy(), colorscale='RdBu', zmid=0)
-                )
+    fig = px.imshow(pred_dc_img)
 
-    fig.update_layout(
-        yaxis=dict(
-            scaleanchor='x',
-            autorange='reversed'
-        ),
-        plot_bgcolor='rgba(0,0,0,0)'
-    )
+    pred_normed_dc = pred_dc.norm(dim=-1)
+
+    # fig = go.Figure(data =
+    #                 go.Heatmap(z = pred_normed_dc.cpu().view(sidelength, sidelength).detach().numpy(), colorscale='RdBu', zmid=0)
+    #             )
+
+    # fig.update_layout(
+    #     yaxis=dict(
+    #         scaleanchor='x',
+    #         autorange='reversed'
+    #     ),
+    #     plot_bgcolor='rgba(0,0,0,0)'
+    # )
 
     mlflow.log_figure(fig, f"prediction_gradients.html")
 
     #----------------------------------------------------------------    
     # Laplacian Prediction
 
-    model_output_dcdc = torch.autograd.grad(outputs=pred_normed_dc.sum(), inputs=coordinates, grad_outputs=None)[0]
+    pred_dcdc = torch.autograd.grad(outputs=pred_normed_dc.sum(), inputs=coordinates, grad_outputs=None)[0]
 
-    pred_normed_dcdc = model_output_dcdc.norm(dim=-1)
+    pred_normed_dcdc = pred_dcdc.norm(dim=-1)
     
     fig = go.Figure(data =
                     go.Heatmap(z = pred_normed_dcdc.cpu().view(sidelength, sidelength).detach().numpy(), colorscale='RdBu', zmid=0)
@@ -119,34 +126,37 @@ def plot_gradients(model, coordinates, ground_truth):
 
     gt_image = ground_truth.reshape(sidelength, sidelength)
 
-    grads_x = scipy.ndimage.sobel(gt_image, axis=0)
-    grads_y = scipy.ndimage.sobel(gt_image, axis=1)
-    grads_x, grads_y = torch.from_numpy(grads_x), torch.from_numpy(grads_y)
-            
-    grads = torch.stack((grads_x, grads_y), dim=-1).view(-1, 2)
-    normed_grad = grads.norm(dim=-1)
+    gt_dcx = scipy.ndimage.sobel(gt_image, axis=0)
+    gt_dcy = scipy.ndimage.sobel(gt_image, axis=1)
+    
+    gt_dc_img = grads2img(gt_dcx, gt_dcy, sidelength)
 
-    fig = go.Figure(data =
-                    go.Heatmap(z = normed_grad.cpu().view(sidelength, sidelength).detach().numpy(), colorscale='RdBu', zmid=0)
-                )
+    fig = px.imshow(gt_dc_img)
 
-    fig.update_layout(
-        yaxis=dict(
-            scaleanchor='x',
-            autorange='reversed'
-        ),
-        plot_bgcolor='rgba(0,0,0,0)'
-    )
+    # gt_dc = torch.stack((gt_dcx, gt_dcy), dim=-1).view(-1, 2)
+    # gt_normed_dc = gt_dc.norm(dim=-1)
+
+    # fig = go.Figure(data =
+    #                 go.Heatmap(z = gt_normed_dc.cpu().view(sidelength, sidelength).detach().numpy(), colorscale='RdBu', zmid=0)
+    #             )
+
+    # fig.update_layout(
+    #     yaxis=dict(
+    #         scaleanchor='x',
+    #         autorange='reversed'
+    #     ),
+    #     plot_bgcolor='rgba(0,0,0,0)'
+    # )
 
     mlflow.log_figure(fig, f"gradients.html")
 
     #----------------------------------------------------------------    
     # Laplacian Ground Truth
 
-    laplacian = scipy.ndimage.laplace(gt_image)
+    gt_dcdc = scipy.ndimage.laplace(gt_image)
 
     fig = go.Figure(data =
-                    go.Heatmap(z = laplacian, colorscale='RdBu', zmid=0)
+                    go.Heatmap(z = gt_dcdc, colorscale='RdBu', zmid=0)
                 )
 
     fig.update_layout(
@@ -162,4 +172,25 @@ def plot_gradients(model, coordinates, ground_truth):
     return {
     }
 
-    
+
+def grads2img(grads_x, grads_y, sidelength):
+    """
+    From https://github.com/vsitzmann/siren/blob/master/dataio.py#L55
+    """
+    grads_x, grads_y = torch.from_numpy(grads_x), torch.from_numpy(grads_y)
+            
+    grads_a = np.arctan2(grads_y, grads_x)
+    grads_m = np.hypot(grads_y, grads_x)
+    grads_hsv = np.zeros((sidelength, sidelength, 3), dtype=np.float32)
+    grads_hsv[:,:,0] = (grads_a + np.pi) / (2 * np.pi)
+    grads_hsv[:,:,1] = 1.
+
+    nPerMin = np.percentile(grads_m, 5)
+    nPerMax = np.percentile(grads_m, 95)
+    grads_m = (grads_m - nPerMin) / (nPerMax - nPerMin)
+    grads_m = np.clip(grads_m, 0, 1)
+
+    grads_hsv[:, :, 2] = grads_m
+    grads_rgb = colors.hsv_to_rgb(grads_hsv)
+
+    return grads_rgb
