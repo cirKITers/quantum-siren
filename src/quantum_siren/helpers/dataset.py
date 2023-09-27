99b13e89e62d380c5a6b50930f49e9b1e2e37b5a
lc3267 <melvin.strobl@kit.edu>
2023-09-27 11:46:31 +0200

    mlflow plotly artifact dataset
    Signed-off-by: lc3267 <melvin.strobl@kit.edu>

--- src/quantum_siren/helpers/dataset.py
+++ src/quantum_siren/helpers/dataset.py
@@ -7,23 +7,20 @@ modo:
   filepath: modo.pt
   model: ExampleModel
 """
-from os.path import isfile
+from os.path import isfile, basename
 from typing import Any, Dict
 import torch
 from kedro.io import AbstractDataSet
 
 from quantum_siren.pipelines.training.models import Model
 
-models = {
-    "model": Model,
-}
+import mlflow
 
-
-class TorchLocalModel(AbstractDataSet):
+class MlFlowPlotlyArtifact(AbstractDataSet):
     def _describe(self) -> Dict[str, Any]:
         return dict(
             filepath=self._filepath,
-            model=self._model,
+            filename=self._filename,
             load_args=self._load_args,
             save_args=self._save_args,
         )
@@ -31,16 +28,50 @@ class TorchLocalModel(AbstractDataSet):
     def __init__(
         self,
         filepath: str,
-        model: str,
         load_args: Dict[str, Any] = None,
         save_args: Dict[str, Any] = None,
     ) -> None:
         self._filepath = filepath
-        self._model = model
-        if model in models:
-            self._Model = Model
-        else:
-            raise KeyError("Add {model} to {models}.")
+        self._filename = basename(filepath)
+        default_save_args = {}
+        default_load_args = {}
+
+        self._load_args = (
+            {**default_load_args, **load_args}
+            if load_args is not None
+            else default_load_args
+        )
+        self._save_args = (
+            {**default_save_args, **save_args}
+            if save_args is not None
+            else default_save_args
+        )
+
+    def _load(self):
+        raise NotImplementedError
+
+    def _save(self, fig) -> None:
+        mlflow.log_figure(fig, self._filename)
+
+    def _exists(self) -> bool:
+        return isfile(self._filepath)
+
+class TorchLocalModel(AbstractDataSet):
+    def _describe(self) -> Dict[str, Any]:
+        return dict(
+            filepath=self._filepath,
+            load_args=self._load_args,
+            save_args=self._save_args,
+        )
+
+    def __init__(
+        self,
+        filepath: str,
+        load_args: Dict[str, Any] = None,
+        save_args: Dict[str, Any] = None,
+    ) -> None:
+        self._filepath = filepath
+        self._Model = Model
         default_save_args = {}
         default_load_args = {}
 
