4239d35fe367a0be032e2839771645310316925c
lc3267 <melvin.strobl@kit.edu>
2023-12-07 10:25:25 +0100

    cpus per task update
    Signed-off-by: lc3267 <melvin.strobl@kit.edu>

--- slurm_job.sh
+++ slurm_job.sh
@@ -1,29 +1,27 @@
 #!/bin/bash
 # 
-# name of the job for better recognizing it in the queue overview
-#SBATCH --job-name=quantum-siren
-# 
 # define how many nodes we need
 #SBATCH --nodes=1
 #
 # we only need on 1 cpu at a time
 #SBATCH --ntasks=1
 #
+#SBATCH --cpus-per-task=16
+#
 # expected duration of the job
 #              hh:mm:ss
-#SBATCH --time=20:00:00
+#SBATCH --time=40:00:00
 # 
 # partition the job will run on
 #SBATCH --partition single
 # 
 # expected memory requirements
-#SBATCH --mem=8000MB
+#SBATCH --mem=72000MB
 #
 # output path
-#SBATCH --output="logs/slurm/slurm-%j.out"
+#SBATCH --output="logs/slurm/slurm-%x-%j.out"
 
-
-./venv/bin/python -m kedro run --params=$1
+/home/kit/scc/lc3267/quantum-siren/.venv/bin/python -m kedro run --params=$1
 
 # Done
 exit 0
