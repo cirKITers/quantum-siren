b87976912096f1bd0eef470226746d84fa1323d7
lc3267 <melvin.strobl@kit.edu>
2024-01-16 15:41:58 +0100

    generic mgrid
    Signed-off-by: lc3267 <melvin.strobl@kit.edu>

--- src/quantum_siren/pipelines/preprocessing/nodes.py
+++ src/quantum_siren/pipelines/preprocessing/nodes.py
@@ -35,14 +35,14 @@ def get_cameraman_tensor(sidelength):
     return img
 
 
-def get_mgrid(sidelen, dim=2):
+def get_mgrid(domain, samples, dim=2):
     """
     Code from https://doi.org/10.48550/arXiv.2006.09661
     """
     """Generates a flattened grid of (x,y,...) coordinates in a range of -1 to 1.
     sidelen: int
     dim: int"""
-    tensors = tuple(dim * [torch.linspace(-1, 1, steps=sidelen)])
+    tensors = tuple(dim * [torch.linspace(domain[0], domain[1], steps=samples)])
     mgrid = torch.stack(torch.meshgrid(*tensors), dim=-1)
     mgrid = mgrid.reshape(-1, dim)
     return mgrid
