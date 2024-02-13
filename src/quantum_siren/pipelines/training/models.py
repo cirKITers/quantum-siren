af96463284c782f710021a6bf486ed49dc7536ec
lc3267 <melvin.strobl@kit.edu>
2024-02-13 12:34:33 +0100

    resolved hard coded output interpretation
    Signed-off-by: lc3267 <melvin.strobl@kit.edu>

--- src/quantum_siren/pipelines/training/models.py
+++ src/quantum_siren/pipelines/training/models.py
@@ -13,13 +13,13 @@ class Model(torch.nn.Module):
     # class Module(torch.nn.Module):
     def __init__(
         self,
-        n_qubits,
-        shots,
-        vqc_ansatz,
-        iec_ansatz,
-        n_layers,
-        data_reupload,
-        output_interpretation,
+        n_qubits: int,
+        shots: int,
+        vqc_ansatz: str,
+        iec_ansatz: str,
+        n_layers: int,
+        data_reupload: bool,
+        output_interpretation: int,
     ) -> None:
         super().__init__()
 
@@ -32,8 +32,8 @@ class Model(torch.nn.Module):
         self.iec = getattr(ansaetze, iec_ansatz, ansaetze.nothing)
         self.vqc = getattr(ansaetze, vqc_ansatz, ansaetze.nothing)
 
-        if output_interpretation != "all":
-            output_interpretation = int(output_interpretation)
+        if output_interpretation > 0:
+            output_interpretation = output_interpretation
             assert output_interpretation < n_qubits, (
                 f"Output interpretation parameter {output_interpretation} "
                 "can either be a qubit (integer smaller n_qubits) or 'all'"
@@ -103,10 +103,10 @@ class Model(torch.nn.Module):
             #     out = pool.starmap(self.qnode, [[params, coord] for coord in model_input])
 
             for i, coord in enumerate(model_input):
-                if self.output_interpretation == "all":
-                    out[i] = torch.mean(self.qlayer(coord), axis=0)
-                else:
+                if self.output_interpretation > 0:
                     out[i] = self.qlayer(coord)[self.output_interpretation]
+                else:
+                    out[i] = torch.mean(self.qlayer(coord), axis=0)
         else:
             out = self.qlayer(model_input)[-1]
 
