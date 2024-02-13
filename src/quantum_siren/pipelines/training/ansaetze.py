8e1fc7ca7ff56748413e9eb3cfec456fda803dfa
lc3267 <melvin.strobl@kit.edu>
2024-02-13 12:33:29 +0100

    added documentation
    Signed-off-by: lc3267 <melvin.strobl@kit.edu>

--- src/quantum_siren/pipelines/training/ansaetze.py
+++ src/quantum_siren/pipelines/training/ansaetze.py
@@ -1,4 +1,6 @@
 import pennylane as qml
+import pennylane.numpy as np
+import torch
 
 
 class ansaetze:
@@ -7,7 +9,7 @@ class ansaetze:
         pass
 
     @staticmethod
-    def circuit_19(params):
+    def circuit_19(params: torch.tensor | np.ndarray):
         """
         Generates a single layer of circuit_19
         See https://arxiv.org/pdf/1905.10876.pdf
@@ -22,23 +24,23 @@ class ansaetze:
         if params is None:
             return 3
 
-        for q, q_params in enumerate(params):
-            qml.RX(q_params[0], wires=q)
-            qml.RZ(q_params[1], wires=q)
+        for qubit, params in enumerate(params):
+            qml.RX(params[0], wires=qubit)
+            qml.RZ(params[1], wires=qubit)
 
-        for q, q_params in enumerate(params):
+        for qubit, params in enumerate(params):
             qml.CRX(
-                q_params[2],
+                params[2],
                 wires=[
-                    params.shape[0] - q - 1,
-                    (params.shape[0] - q) % params.shape[0],
+                    params.shape[0] - qubit - 1,
+                    (params.shape[0] - qubit) % params.shape[0],
                 ],
             )
 
     @staticmethod
-    def circuit_18(params):
+    def circuit_18(params: torch.tensor | np.ndarray):
         """
-        Generates a single layer of circuit_19
+        Generates a single layer of circuit_18
         See https://arxiv.org/pdf/1905.10876.pdf
 
         Args:
@@ -51,29 +53,39 @@ class ansaetze:
         if params is None:
             return 3
 
-        for q, q_params in enumerate(params):
-            qml.RX(q_params[0], wires=q)
-            qml.RZ(q_params[1], wires=q)
+        for qubit, params in enumerate(params):
+            qml.RX(params[0], wires=qubit)
+            qml.RZ(params[1], wires=qubit)
 
-        for q, q_params in enumerate(params):
+        for qubit, params in enumerate(params):
             qml.CRZ(
-                q_params[2],
+                params[2],
                 wires=[
-                    params.shape[0] - q - 1,
-                    (params.shape[0] - q) % params.shape[0],
+                    params.shape[0] - qubit - 1,
+                    (params.shape[0] - qubit) % params.shape[0],
                 ],
             )
 
     @staticmethod
-    def default(params, **kwargs):
-        for q, q_params in enumerate(params):
-            qml.RX(q_params[0], wires=q)
-            qml.RY(q_params[1], wires=q)
+    def default(params: torch.tensor | np.ndarray):
+        """Encoding of two dimensional data using RX and RY gates. The input is repeated across all qubits (vertically), as specified by the shape of the input.
+
+        Args:
+            params (torch.tensor | np.ndarray): Input data with the first value parameterizing the RX gate and the second value parameterizing the RY gate. Expects form to be [n_qubits, 2]
+        """
+        for qubit, params in enumerate(params):
+            qml.RX(params[0], wires=qubit)
+            qml.RY(params[1], wires=qubit)
 
     @staticmethod
-    def spread_layers(params, **kwargs):
-        for q, q_params in enumerate(params):
-            if 2 * q + 1 > params.shape[0] - 1:
+    def spread_layers(params: torch.tensor | np.ndarray):
+        """Encoding of two dimensional data using RY gates interleaving across the qubits. Here, the first qubits takes the first parameter, the second the second, the third one the first again and so on.
+
+        Args:
+            params (torch.tensor | np.ndarray): Input data with the first value parameterizing the RX gate and the second value parameterizing the RY gate. Expects form to be [n_qubits, 2]
+        """
+        for qubit, params in enumerate(params):
+            if 2 * qubit + 1 > params.shape[0] - 1:
                 break
-            qml.RY(q_params[0], wires=2 * q)
-            qml.RY(q_params[1], wires=2 * q + 1)
+            qml.RY(params[0], wires=2 * qubit)
+            qml.RY(params[1], wires=2 * qubit + 1)
