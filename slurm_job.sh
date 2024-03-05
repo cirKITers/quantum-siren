4b18dbf9d045a87d16fcb0077da25d03fd47dc4d
lc3267 <melvin.strobl@kit.edu>
2024-03-05 09:46:58 +0100

    adjusted specs
    Signed-off-by: lc3267 <melvin.strobl@kit.edu>

--- slurm_job.sh
+++ slurm_job.sh
@@ -6,22 +6,22 @@
 # we only need on 1 cpu at a time
 #SBATCH --ntasks=1
 #
-#SBATCH --cpus-per-task=16
+#SBATCH --cpus-per-task=8
 #
 # expected duration of the job
 #              hh:mm:ss
-#SBATCH --time=40:00:00
+#SBATCH --time=10:00:00
 # 
 # partition the job will run on
 #SBATCH --partition single
 # 
 # expected memory requirements
-#SBATCH --mem=72000MB
+#SBATCH --mem=20000MB
 #
 # output path
-#SBATCH --output="logs/slurm/slurm-%x-%j.out"
+#SBATCH --output="logs/slurm/slurm-%j-%x.out"
 
-/home/kit/scc/lc3267/quantum-siren/.venv/bin/python -m kedro run --params=$1
+/home/kit/scc/lc3267/quantum-siren/.venv/bin/python -m kedro run --pipeline cluster --params=$1
 
 # Done
 exit 0
