ab59e2433fb81d7a31f45ae397cceb134e849ae9
stroblme <melvin.strobl@kit.edu>
2024-05-13 21:52:22 +0200

    adjusted to latest kedro
    Signed-off-by: stroblme <melvin.strobl@kit.edu>

--- eval_n_qubits_n_layers.sh
+++ eval_n_qubits_n_layers.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-for n_qubits in 2 4 6 8; do
-    for n_layers in 2 6 10 14 18 22; do
-        sbatch --job-name "qs-q$n_qubits-l$n_layers" ./slurm_job.sh "training.n_qubits:$n_qubits,training.n_layers:$n_layers" "slurm"
+for n_qubits in 2 3 4 5 6 7; do
+    for n_layers in 2 4 6 8 10 12; do
+        sbatch --job-name "qs-q$n_qubits-l$n_layers" ./slurm_job.sh "training.n_qubits=$n_qubits,training.n_layers=$n_layers" "slurm"
     done
 done
 # Done
