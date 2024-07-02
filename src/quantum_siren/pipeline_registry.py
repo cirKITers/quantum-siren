b60fd27d69de87fc8ee756f1d9864fe94a34f50b
Melvin Strobl <lc3267@kit.edu>
2024-07-02 14:43:10 +0200

    explicit pipeline creation
    Signed-off-by: Melvin Strobl <lc3267@kit.edu>

--- src/quantum_siren/pipeline_registry.py
+++ src/quantum_siren/pipeline_registry.py
@@ -5,6 +5,16 @@ from __future__ import annotations
 from kedro.framework.project import find_pipelines
 from kedro.pipeline import Pipeline
 
+from quantum_siren.pipelines.preprocessing.pipeline import (
+    create_pipeline as create_preprocessing_pipeline,
+)
+from quantum_siren.pipelines.training.pipeline import (
+    create_pipeline as create_training_pipeline,
+)
+from quantum_siren.pipelines.postprocessing.pipeline import (
+    create_pipeline as create_postprocessing_pipeline,
+)
+
 
 def register_pipelines() -> dict[str, Pipeline]:
     """Register the project's pipelines.
@@ -12,7 +22,10 @@ def register_pipelines() -> dict[str, Pipeline]:
     Returns:
         A mapping from pipeline names to ``Pipeline`` objects.
     """
-    pipelines = find_pipelines()
-    pipelines["__default__"] = sum(pipelines.values())
-    pipelines["slurm"] = pipelines["preprocessing"] + pipelines["training"]
+    pipelines = {
+        "__default__": create_preprocessing_pipeline()
+        + create_training_pipeline()
+        + create_postprocessing_pipeline(),
+        "slurm": create_preprocessing_pipeline() + create_training_pipeline(),
+    }
     return pipelines
